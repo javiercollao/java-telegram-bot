1e09c57e5805b96cd859b44522f5995c0a9d7597
Javier Collao <javier.collao@usach.cl>
2023-12-21 00:54:48 -0300

    add btns and images for game

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     public static final String NAME = "MyBotDeMaraton_bot";
@@ -16,17 +20,79 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        // TODO: escribiremos la funcionalidad principal del bot aquí
-        if(update.hasMessage() && update.getMessage().getText().equals("/start")){
-            sendTextMessageAsync("Comencemos _cursiva_ *hola* ");
+        if(getMessageText().equals("/start")){
+            setUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
+            sendTextMessageAsync(STEP_1_TEXT,
+                    Map.of("Hackear la nevera", "step_1_btn")
+            );
         }
 
-        if(update.hasMessage() && update.getMessage().getText().contains("hola") || update.getMessage().getText().contains("Hola") ){
-            sendTextMessageAsync("Hola compadre!");
+        if (getCallbackQueryButtonKey().equals("step_1_btn")){
+            setUserGlory(20);
+            sendPhotoMessageAsync("step_2_pic");
+            sendTextMessageAsync(STEP_2_TEXT,
+                    Map.of("¡Tomar una salchicha! +20 de fama\n","step_2_btn",
+                            "¡Tomar un pescado! +20 de fama\n","step_2_btn",
+                            "¡Tirar una lata de pepinillos! +20 de fama", "step_2_btn")
+            );
         }
 
-        if(update.hasMessage() && update.getMessage().getText().contains("chao") || update.getMessage().getText().contains("Chao") ){
-            sendTextMessageAsync("nos vemos compadre!");
+        if (getCallbackQueryButtonKey().equals("step_2_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_3_pic");
+            sendTextMessageAsync(STEP_3_TEXT,
+                    Map.of("al robot aspirador", "step_3_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_3_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_4_pic");
+            sendTextMessageAsync(STEP_4_TEXT,
+                    Map.of("Enviar al robot aspirador a por comida! +30 de fama\n", "step_4_btn",
+                            "Dar un paseo en el robot aspirador! +30 de fama\n","step_4_btn",
+                            "¡Huir del robot aspirador! +30 de fama", "step_4_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_4_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_5_pic");
+            sendTextMessageAsync(STEP_5_TEXT,
+                    Map.of("Encender y ponerse la GoPro!", "step_5_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_5_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_6_pic");
+            sendTextMessageAsync(STEP_6_TEXT,
+                    Map.of("¡Correr por los tejados, grabar con la GoPro! +40 de fama\n", "step_6_btn",
+                            "¡Atacar a otros gatos desde tu escondite con la GoPro! +40 de fama\n", "step_6_btn",
+                            "¡Atacar a los perros desde tu escondite con la GoPro! +40 de fama", "step_6_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_6_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_7_pic");
+            sendTextMessageAsync(STEP_7_TEXT,
+                    Map.of("Hackear la contraseña", "step_7_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_7_btn")) {
+            addUserGlory(30);
+            sendPhotoMessageAsync("step_8_pic");
+            sendTextMessageAsync(STEP_8_TEXT,
+                    Map.of("Salir al patio", "step_8_btn")
+            );
+        }
+
+        if (getCallbackQueryButtonKey().equals("step_8_btn")) {
+            sendPhotoMessageAsync("final_pic");
+            sendTextMessageAsync(FINAL_TEXT);
         }
 
     }
